5de73d619ef6514aaead41229689212284d08df4
Koen <koen@denooij.com>
2020-12-14 03:01:43 +0100

    Added basic project structure

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'arrow-body-style': ['off'],
+    'react/destructuring-assignment': ['off'],
+    "no-plusplus": "off",
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "react/prefer-stateless-function": "off",
+    "react/prop-types": "off"
+  }
+};
