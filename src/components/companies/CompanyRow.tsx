919ec2cb4ab455fbf2216f7a044902d120c3ec65
Max <m.opperman98@gmail.com>
2020-12-21 13:59:48 +0100

    Create CompanyRow.tsx

---
+++ src/components/companies/CompanyRow.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Table } from 'semantic-ui-react';
+import { Company, CompanyStatus } from '../../clients/server.generated';
+import { formatPriceFull } from '../../helpers/monetary';
+
+interface Props {
+  company: Company;
+}
+
+export function CompanyRow(props: Props) {
+  const { company } = props;
+  return (
+    <Table.Row>
+      <Table.Cell>
+        <NavLink to={`/company/${company.id}`}>
+          {company.name}
+        </NavLink>
+      </Table.Cell>
+      <Table.Cell>
+        <NavLink to={`/product/${company.id}`}>
+          {company.description}
+        </NavLink>
+      </Table.Cell>
+      <Table.Cell>
+        <NavLink to={`/product/${company.id}`}>
+          {company.description}
+        </NavLink>
+      </Table.Cell>
+      <Table.Cell>
+        {company.status === CompanyStatus.ACTIVE ? 'Active' : 'Inactive'}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
