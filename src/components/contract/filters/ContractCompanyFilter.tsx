e009e591b78384e3d10d4e182facd1987b96ef82
Max <m.opperman98@gmail.com>
2021-01-02 11:05:55 +0100

    Implement ContractCompanyFilter

---
+++ src/components/contract/filters/ContractCompanyFilter.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { CompanySummary } from '../../../clients/server.generated';
+import { RootState } from '../../../stores/store';
+import { getSummaryCollection } from '../../../stores/summaries/selectors';
+import { SummaryCollections } from '../../../stores/summaries/summaries';
+import { Tables } from '../../../stores/tables/tables';
+import ColumnFilter from '../../ColumnFilter';
+
+interface Props {
+  options: CompanySummary[],
+}
+
+function ContractCompanyFilter(props: Props) {
+  return (
+    <ColumnFilter
+      column="companyId"
+      columnName="Company"
+      table={Tables.Contracts}
+      options={props.options.map((o) => ({ value: o.id, key: o.id, text: o.name }))}
+    />
+  );
+}
+
+const mapStateToProps = (state: RootState) => ({
+  options: getSummaryCollection<CompanySummary>(
+    state, SummaryCollections.Companies,
+  ).options,
+});
+
+export default connect(mapStateToProps)(ContractCompanyFilter);
