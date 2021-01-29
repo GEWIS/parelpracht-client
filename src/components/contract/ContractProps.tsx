76b79102a7a8752a7754d5f889fe8a8127076485
Max <m.opperman98@gmail.com>
2021-01-29 08:52:48 +0100

    Revert "Fixed the tabbing with some help of Roy"
    This reverts commit 1c3cce4534fe4a1b3c03d6a50c1e262dc4428302.

--- src/components/contract/ContractProps.tsx
+++ src/components/contract/ContractProps.tsx
@@ -150,7 +150,6 @@ class ContractProps extends React.Component<Props, State> {
           <CompanySelector
             id="form-company-selector"
             value={companySelection}
-            disabled
             onChange={(val: number) => this.setState({
               companySelection: val,
             })}
