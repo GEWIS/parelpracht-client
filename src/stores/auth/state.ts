749857eaf8e774ee6ea85edf3806b1d2dba1422a
Koen de Nooij <koen@denooij.com>
2021-03-20 15:20:12 +0100

    Implemented requesting and revoking API Keys on your profile

--- src/stores/auth/state.ts
+++ src/stores/auth/state.ts
@@ -1,12 +1,15 @@
-import { AuthStatus, User } from '../../clients/server.generated';
+import { AuthStatus, Profile } from '../../clients/server.generated';
 import ResourceStatus from '../resourceStatus';
 
 export interface AuthState {
   authStatus: AuthStatus | undefined;
   status: ResourceStatus;
 
-  profile: User | undefined;
+  profile: Profile | undefined;
   profileStatus: ResourceStatus;
 
   passwordRequest: ResourceStatus;
+
+  apiKey: string | undefined;
+  apiKeyRequest: ResourceStatus;
 }
