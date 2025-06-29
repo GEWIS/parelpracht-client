500d22a86f17ff3050aea17d79dfb7c5d9db66eb
Roy Kakkenberg <me@roykakkenberg.nl>
2025-06-29 14:41:27 +0200

    chore: remove dev branch from actions

--- release.config.js
+++ release.config.js
@@ -4,6 +4,6 @@ import  { releaseConfig } from '@gewis/release-config';
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    branches: ["main", "ci/github-actions"],
+    branches: ["main"],
     ...releaseConfig
 };
