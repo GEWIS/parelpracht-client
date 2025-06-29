4a4c0728d532d09a5f700c8ccdd550a9c6d31467
Roy Kakkenberg <me@roykakkenberg.nl>
2025-06-29 12:33:45 +0200

    ci: add semantic release config

---
+++ release.config.js
@@ -0,0 +1,1 @@
+import  { releaseConfig } from '@gewis/release-config';/** * @type {import('semantic-release').GlobalConfig} */export default {    branches: ["main", "ci/github-actions"],    ...releaseConfig};
