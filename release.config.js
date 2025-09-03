597eb53bb61a022ccca1411db9a931e09625e119
Roy Kakkenberg <me@roykakkenberg.nl>
2025-09-03 22:00:32 +0200

    feat: auto update changelog on release using @semantic-release/changelog

--- release.config.js
+++ release.config.js
@@ -4,6 +4,21 @@ import  { releaseConfig } from '@gewis/release-config';
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    branches: ["main", "feat/update-changelog-on-release"],
-    ...releaseConfig
+  branches: ["main", "feat/update-changelog-on-release"],
+  ...releaseConfig,
+  plugins: [
+    ...releaseConfig.plugins,
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "parelpracht-client/src/changelog.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["parelpracht-client/src/changelog.md"]
+      }
+    ]
+  ]
 };
